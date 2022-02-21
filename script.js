fcfc9388dd30b645039a3794dccf699a608fb836
user-pc03 <katerina.yad.24@gmail.com>
2022-02-21 18:00:57 +0400

    make clock

---
+++ script.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("sandbox");
+var context = canvas.getContext("2d");
+var square, circle, letter;
+
+// context.fillRect(48, 48, 204 204);
+
+// square = new Path2D();
+// square.moveTo(48, 48);
+// square.lineTo(96, 48);
+// square.lineTo(96, 120);
+// square.lineTo(160, 48);
+// square.lineTo(208, 48);
+// square.lineTo(96, 168);
+// square.lineTo(208, 280);
+// square.lineTo(160, 280);
+// square.lineTo(96, 216);
+// square.lineTo(96, 280);
+// square.lineTo(48, 280);
+// square.lineTo(48, 48);
+// context.stroke(square)
+
+// circle = new Path2D();
+// circle.arc(150, 150, 148, 0, 2 * Math.PI);
+// context.fill(circle)
+
+context.fillStyle = "darkorange";
+context.fillRect(48, 48, 206, 206);
+
+context.fillStyle = "rgba(248, 160, 96, 1.5)";
+context.fillRect(96, 96, 206, 206);
